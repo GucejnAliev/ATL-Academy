b440c81c9a567e02e7e97f3469105418a62dc111
GucejnAliev <gucejnaliev@gmail.com>
2024-11-18 22:14:23 +0400

    Add ReverseName.java

---
+++ src/hometasks/ReverseName.java
@@ -0,0 +1,24 @@
+package hometasks;
+
+import java.util.Scanner;
+
+public class ReverseName {
+
+    public static String reverseString(String input) {
+
+        StringBuilder reversed = new StringBuilder(input);
+        return reversed.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Adınızı daxil edin: ");
+        String name = scanner.nextLine();
+
+        String reversedName = reverseString(name);
+
+        System.out.println(reversedName);
+
+    }
+}
